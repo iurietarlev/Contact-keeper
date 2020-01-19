784eeaff388a28f12a497a87e9774c0e1148b7fd
osk849 <iura.tarlev@gmail.com>
2020-01-19 19:14:45 +0000

    edited landing page when logging out

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = ({ title, icon }) => {
     <Fragment>
       <li>Hello {user && user.name} </li>
       <li>
-        <a onClick={onLogout} href="!#">
+        <a onClick={onLogout} href="login#!">
           <i className="fas fa-sign-out-alt" />{' '}
           <span className="hide-sm">Logout</span>
         </a>
